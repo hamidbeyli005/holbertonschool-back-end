5417c38979b7415f13b7a4ed430ed708f632e8d6
Hamid Hamidbayli <hemidbeyli2020@gmail.com>
2024-04-25 22:50:08 +0400

    Updated

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,49 +1,25 @@
 #!/usr/bin/python3
 """Gather data from an API"""
 
-
 import requests
 import sys
 
 
-def get_todo_list_progress(employee_id):
-    base_url = "https://jsonplaceholder.typicode.com"
-    employee_url = f"{base_url}/users/{employee_id}"
-    todo_url = f"{base_url}/todos?userId={employee_id}"
-
-    try:
-        # Fetch employee data
-        employee_response = requests.get(employee_url)
-        employee_response.raise_for_status()
-        employee_data = employee_response.json()
-
-        # Fetch TODO list data
-        todo_response = requests.get(todo_url)
-        todo_response.raise_for_status()
-        todo_data = todo_response.json()
-
-        # Extract relevant information
-        employee_name = employee_data["name"]
-        completed_tasks = [task for task in todo_data if task["completed"]]
-        number_of_done_tasks = len(completed_tasks)
-        total_number_of_tasks = len(todo_data)
-
-        # Display information
-        print(
-            f"Employee {employee_name} is done with tasks(
-                {number_of_done_tasks}/{total_number_of_tasks}): ")
-        for task in completed_tasks:
-            print(f"\t{task['title']}")
-
-    except requests.exceptions.RequestException as e:
-        print("Error fetching data:", e)
-        sys.exit(1)
+def get_data():
+    """get data """
+    user_id = sys.argv[1]
+    users_route = 'https://jsonplaceholder.typicode.com/users/{}'
+    todos_route = 'https://jsonplaceholder.typicode.com/todos/?userId={}'
+    user = users_route.format(user_id)
+    todos = todos_route.format(user_id)
+    name = requests.get(user).json().get('name')
+    todos_request = requests.get(todos).json()
+    tasks = [task.get('title') for task in todos_request
+             if task.get('completed') is True]
+    print('Employee {} is done with tasks({}/{}):'.format(name,
+          len(tasks), len(todos_request)))
+    print('\n'.join('\t {}'.format(task) for task in tasks))
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
-        sys.exit(1)
-
-    employee_id = sys.argv[1]
-    get_todo_list_progress(employee_id)
+    get_data()
